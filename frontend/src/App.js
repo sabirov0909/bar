0aa506493f7e1f5f76fc8202e985a922de2abc87
dalersabirov <sobrovv08@gmail.com>
2025-12-14 21:31:19 +0500

    done task 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+    const [count, setCount] = useState(0)
     return (
         <div>
-            hello world
-            <h1> fndlk</h1>
+            <p>Cчет: {count}</p>
+            <button className='count' onClick={() => setCount(count + 1)}>+</button>
+            <button className='count' onClick={() => setCount(count - 1)}>-</button>
         </div>
     );
 };
